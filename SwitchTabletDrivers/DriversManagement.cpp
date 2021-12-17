d18550a4c4cc29e4f203322196f7957303778475
kiritovex <scutogaetano@gmail.com>
2021-12-17 15:37:38 +0100

    Aggiornamento struttura codice. v.0.6

--- SwitchTabletDrivers/DriversManagement.cpp
+++ SwitchTabletDrivers/DriversManagement.cpp
@@ -1,4 +1,11 @@
-#include "DriversManagement.h"
+#include    "DriversManagement.h"
+#include    <tlhelp32.h>
+#include    <filesystem>
+#include    <fstream>
+#include    <algorithm>
+
+
+using namespace Management;
 
 BOOL Management::Service::SetPrivilege(
     HANDLE hToken,          // access token handle
@@ -48,4 +55,122 @@ BOOL Management::Service::SetPrivilege(
     } 
 
     return TRUE;
+}
+
+void Service::ServiceManagement(string szSvcName[],int j){
+    
+        SERVICE_STATUS Status;
+
+        SC_HANDLE SCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+        SC_HANDLE SHandle = OpenService(SCManager, szSvcName[j].c_str(), SC_MANAGER_ALL_ACCESS);
+
+        if(SHandle == NULL)
+        {
+            cout <<"|" << "ERROR " << GetLastError() << endl;
+        }
+        
+        else if(!ControlService(SHandle, SERVICE_CONTROL_STOP, &Status))
+        {
+            cout <<"|" << "FAILED TO SEND STOP SERVICE COMMAND: " << GetLastError()<<endl;
+        }
+        else
+        {
+            cout <<"|" << "Service stopped successfully"<<endl;
+        }
+ 
+        do
+        {
+            QueryServiceStatus(SHandle, &Status);
+            //cout <<"|" << "Checking Service Status...\n";
+        }while(Status.dwCurrentState != SERVICE_STOPPED);   
+
+        if(!StartService(SHandle, 0, NULL))
+        {
+            cout <<"|" << "Service Did Not Start Up: " << GetLastError() << endl;
+        }
+        else
+        {
+            cout <<"|" << "Service Started successfully"<<endl;
+        }
+        
+        CloseServiceHandle(SCManager);
+        CloseServiceHandle(SHandle);
+}
+
+void Process::ProcessController(string ProcessName) {
+        PROCESSENTRY32 entry;
+        entry.dwSize = sizeof(PROCESSENTRY32);
+
+        const auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+        if (!Process32First(snapshot, &entry)) {
+            CloseHandle(snapshot);
+        }
+        
+        do {
+            if (!strcmp(entry.szExeFile, ProcessName.c_str() )) {
+                
+                cout <<endl<<"|" <<ProcessName<< " is running"<< endl;
+                //chiusura processo
+                cout<<"|Closing..."<<endl<<endl;
+                HANDLE hProcess = OpenProcess(PROCESS_TERMINATE,FALSE,entry.th32ProcessID); 
+
+                TerminateProcess(hProcess,0);
+                CloseHandle(hProcess);
+            }
+        } while (Process32Next(snapshot, &entry));
+
+        CloseHandle(snapshot);
+
+}
+
+void Process::ProcessStartup(string pathProcess){
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+    LPSTR CPath = const_cast<char *>(pathProcess.c_str());
+
+    if( !CreateProcessA( NULL,   // No module name (use command line)
+        CPath,        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi )           // Pointer to PROCESS_INFORMATION structure
+    ){
+        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        return;
+    }  
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+}
+
+bool Process::ProcessChecker(string Process){
+
+        PROCESSENTRY32 entry;
+        entry.dwSize = sizeof(PROCESSENTRY32);
+
+        const auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+        if (!Process32First(snapshot, &entry)) {
+            CloseHandle(snapshot);
+            return false;
+        }
+        do {
+            if (!strcmp(entry.szExeFile, Process.c_str() )) {
+                    CloseHandle(snapshot);
+                    return true;
+            }
+        } while (Process32Next(snapshot, &entry));
+        
+        CloseHandle(snapshot);
+        return false;
 }
